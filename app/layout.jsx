613271307e34625224f79bbe5623f9c1ba9727af
Vikram-VN <vikram.narayanappa@apexit.com>
2023-05-03 17:17:20 +0530

    fixed icons

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,11 @@
-import { Inter, Preahvihear } from 'next/font/google';
+"use client";
+import { Inter } from 'next/font/google';
 import Footer from './components/footer/footer';
 import Header from './components/header/header';
+import SideBar from './components/navbar/sideBar';
+import { useEffect, useState } from "react";
 import 'flowbite';
 import './globals.css';
-import SideBar from './components/navbar/sideBar';
-import PreviousTheme from './components/header/components/theme/previousTheme';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,14 +15,39 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+
+  const [theme, setTheme] = useState(
+    typeof window !== "undefined" ? localStorage.theme : "dark"
+  );
+  const colorTheme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", theme);
+    }
+  }, [colorTheme, theme]);
+
+  const changeTheme = () => {
+    const colorTheme = theme === "dark" ? "light" : "dark";
+    setTheme(colorTheme);
+  }
+
   return (
-    <html lang="en">
+
+    <html lang="en" className={theme}>
       <head>
-        <PreviousTheme />
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className={inter.className}>
         <section className="flex flex-col h-screen">
-          <Header />
+          <Header theme={theme} changeTheme={changeTheme} />
           <section className="flex bg-white text-black dark:bg-slate-900 dark:text-white">
             <SideBar />
             <section className="p-10 w-full">{children}</section>
