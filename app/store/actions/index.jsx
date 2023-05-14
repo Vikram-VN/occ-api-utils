3015640e7894a6bbb408f82720565ba5f306541c
/ /\/ <vikramn06081999@gmail.com>
2023-05-14 18:57:40 +0530

    nothing is working

--- app/store/actions/index.jsx
+++ app/store/actions/index.jsx
@@ -1,8 +1,16 @@
+<<<<<<< HEAD
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { noop } from '../../utils';
 import httpCall from '../../utils/httpCall';
 
 function* apiHandler(action) {
+=======
+import { put, takeEvery } from 'redux-saga/effects';
+import { noop } from '../../utils';
+import httpCall from '../../utils/httpCall';
+
+function* sagasHandler(action) {
+>>>>>>> 5dde92b (nothing is working)
   try {
     const requestEndpoint = action.payload.url || '/';
     const requestMethod = action.payload.method || 'get';
