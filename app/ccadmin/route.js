eef43547ce7f303d3ffabad097371363cd73815f
Vikram-VN <vikram.narayanappa@apexit.com>
2023-05-18 15:58:58 +0530

    updated

--- app/ccadmin/route.js
+++ app/ccadmin/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
 
-  return NextResponse.json({ msg: 'NexJS server is running!' });
+  return NextResponse.json({ msg: 'OCC server is running!' });
 
 }
--- app/file/route.js
+++ app/file/route.js
@@ -2,6 +2,6 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
 
-  return NextResponse.json({ msg: 'NexJS files API is running!' });
+  return NextResponse.json({ msg: 'Files API is running!' });
 
 }
