69743973190042bb3c58695d9999f945f0e6cad2
/ /\/ <vikramn06081999@gmail.com>
2023-05-23 21:09:33 +0530

    fixed type issue

--- app/profiles/page.jsx
+++ app/profiles/page.jsx
@@ -120,7 +120,7 @@ export default function Profiles() {
           <div className="text-center">
             <ExclamationCircleIcon className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
             <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-              Are you sure you want to contact?
+              Are you sure you want delete contact?
             </h3>
             <div className="flex justify-center gap-4">
               <Button color="failure" onClick={profileDelete}>
