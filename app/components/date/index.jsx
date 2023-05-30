42723fb0388316a28bca39dce09e97d69a986c17
/ /\/ <vikramn06081999@gmail.com>
2023-05-30 20:53:21 +0530

    added server logs

--- app/components/date/index.jsx
+++ app/components/date/index.jsx
@@ -1,48 +1,23 @@
-import React, { useState } from "react";
-import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/solid";
-import { default as DateSelector } from "tailwind-datepicker-react";
+import React from "react";
+import { default as DateSelector } from "react-tailwindcss-datepicker";
+import { noop } from "../../utils";
 
-const DatePicker = (props) => {
-
-    const [show, setShow] = useState(false)
-    const handleChange = (selectedDate) => {
-        console.log(selectedDate)
-    }
-    const handleClose = (state) => {
-        setShow(state)
-    }
-
-
-    const options = {
-        title: "Date",
-        autoHide: true,
-        todayBtn: true,
-        clearBtn: true,
-        maxDate: new Date(),
-        minDate: new Date("2000-01-01"),
-        theme: {
-            background: "bg-gray-700 dark:bg-gray-800",
-            todayBtn: "",
-            clearBtn: "",
-            icons: "",
-            text: "",
-            disabledText: "bg-gray-500",
-            input: "",
-            inputIcon: "",
-            selected: "",
-        },
-        icons: {
-            prev: () => <ChevronLeftIcon className="w-6 h-6"/>,
-            next: () => <ChevronRightIcon className="w-8 h-8"/>,
-        },
-        datepickerClassNames: "top-12",
-        defaultDate: new Date(),
-        language: "en",
-        ...props.options
-    }
+const DatePicker = ({ handleValueChange = noop, ...props }) => {
 
     return (
-        <DateSelector options={options} onChange={handleChange} show={show} setShow={handleClose} />
+        <DateSelector
+            asSingle={true}
+            useRange={false}
+            placeholder="Select date"
+            minDate={new Date("2010-01-01")}
+            maxDate={new Date()}
+            onChange={handleValueChange}
+            configs={{
+                today: "Today",
+                yesterday: "Yesterday"
+            }}
+            {...props}
+        />
     )
 }
 
