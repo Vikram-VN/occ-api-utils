352268ea4264ef65b7bd89d7cb39740ba50e1aff
/ /\/ <vikramn06081999@gmail.com>
2023-06-04 21:40:47 +0530

    added images

--- app/tools/page.jsx
+++ app/tools/page.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Excel2Json from "./fileConvert";
 import { Accordion, Card } from "flowbite-react";
+import Image from "next/image";
 
 export default function Tools(props) {
 
@@ -19,9 +20,11 @@ export default function Tools(props) {
             JSON to XLSX and XLSX to JSON.
           </Accordion.Title>
           <Accordion.Content>
-            <h1 className="text-2xl mb-4">Convert XLSX to JSON</h1>
+            <h1 className="text-2xl mb-4 inline-block">Convert XLSX to JSON</h1>
+            <Image src="media/Excel-to-JSON.png" height={100} width={100} alt="XLSX to JSON" className="inline ml-4"/>
             <Excel2Json label="Download JSON" fileType="xlsx2Json" />
-            <h1 className="text-2xl mt-10 mb-4">Convert JSON to XLSX</h1>
+            <h1 className="text-2xl mt-10 mb-4 inline-block">Convert JSON to XLSX</h1>
+            <Image src="media/JSON-to-Excel.png" height={100} width={100} alt="JSON to XLSX" className="inline ml-4"/>
             <Excel2Json label="Download XLSX" fileType="json2Xlsx" />
           </Accordion.Content>
         </Accordion.Panel>
@@ -31,9 +34,11 @@ export default function Tools(props) {
             XLSX to CSV and CSV to XLSX
           </Accordion.Title>
           <Accordion.Content>
-            <h1 className="text-2xl mt-10 mb-4">Convert XLSX to CSV</h1>
+            <h1 className="text-2xl mt-10 mb-4 inline-block">Convert XLSX to CSV</h1>
+            <Image src="media/XLSX-to-CSV.png" height={120} width={120} alt="XLSX to CSV" className="inline ml-4"/>
             <Excel2Json label="Download CSV" fileType="xlsx2Csv" />
-            <h1 className="text-2xl mt-10 mb-4">Convert CSV to XLSX</h1>
+            <h1 className="text-2xl mt-10 mb-4 inline-block">Convert CSV to XLSX</h1>
+            <Image src="media/CSV-to-XLSX.png" height={100} width={100} alt="CSV to XLSX" className="inline ml-4"/>
             <Excel2Json label="Download XLSX" fileType="csv2Xlsx" />
           </Accordion.Content>
         </Accordion.Panel>
