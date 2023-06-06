7fde24390ae61cef8e7dd947c8db03c1156fdb7f
Vikram-VN <vikram.narayanappa@apexit.com>
2023-06-06 16:03:20 +0530

    added json editor

--- app/tools/json-editor/index.jsx
+++ app/tools/json-editor/index.jsx
@@ -1,60 +1,32 @@
 "use client";
+import dynamic from "next/dynamic";
 import React, { useState } from "react";
-import VanillaJSONEditor from "./jsonEditor";
+
+const VanillaJSONEditor = dynamic(() => import("./jsonEditor"), { ssr: false })
+
 
 const JsonEditor = () => {
 
-    const [showEditor, setShowEditor] = useState(true);
-    const [readOnly, setReadOnly] = useState(false);
-    const [content, setContent] = useState({
-        json: {
-            
-        }
-    });
+    const [content, setContent] = useState({ json: { msg: "Hello world!" } });
 
     return (
         <div className="justify">
-            <h1>JSON Editor in React</h1>
-            <div className="mt-4">
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={showEditor}
-                        onChange={() => setShowEditor(!showEditor)}
+            <div className="flex phone:flex-row flex-col gap-6 mt-6">
+                <div className="w-full">
+                    <h2>Editor</h2>
+                    <VanillaJSONEditor
+                        content={content}
+                        onChange={setContent}
                     />
-                    Show JSON editor
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={readOnly}
-                        onChange={() => setReadOnly(!readOnly)}
-                    />
-                    Read only
-                </label>
-            </div>
-
-            <div className="flex phone:flex-col mt-6">
-
-                {showEditor && (
-                    <div>
-                        <h2>Editor</h2>
-                        <div className="my-editor">
-                            <VanillaJSONEditor
-                                content={content}
-                                readOnly={readOnly}
-                                onChange={setContent}
-                            />
-                        </div>
-                    </div>
-                )}
-                <div>
+                </div>
+                <div className="flex justify-center items-center">
+                    Items
+                </div>
+                <div className="w-full">
                     <h2>Contents</h2>
                     <VanillaJSONEditor
                         content={content}
-                        readOnly={readOnly}
+                        readOnly={true}
                         onChange={setContent}
                     />
                 </div>
