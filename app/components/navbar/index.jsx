2889dc1662bfd9dcc2aab4589160164bfe9b2ecf
Vikram-VN <vikram.narayanappa@apexit.com>
2023-07-19 19:59:16 +0530

    Added item types

--- app/components/navbar/index.jsx
+++ app/components/navbar/index.jsx
@@ -10,7 +10,8 @@ import {
    UsersIcon,
    MagnifyingGlassIcon,
    WrenchScrewdriverIcon,
-   BuildingStorefrontIcon
+   BuildingStorefrontIcon,
+   CubeIcon
 } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { Tooltip } from "flowbite-react";
@@ -63,6 +64,13 @@ const NavBar = () => {
                   <Link href="/import" className="h-10 mr-4 flex items-center p-2 mt-2 ml-3 text-sm text-slate-900 rounded-lg dark:text-white hover:bg-slate-100 dark:hover:bg-slate-700">
                      <ArrowDownTrayIcon className="w-6 h-6 text-slate-600 transition duration-75 dark:text-slate-400 group-hover:text-slate-900 dark:group-hover:text-white" fill="currentColor" stroke="currentColor" />
                      <span className="sr-only">Import</span>
+                  </Link>
+               </Tooltip>
+               
+               <Tooltip content="Item Types" placement="auto" className="z-20">
+                  <Link href="/items" className="h-10 mr-4 flex items-center p-2 mt-2 ml-3 text-sm text-slate-900 rounded-lg dark:text-white hover:bg-slate-100 dark:hover:bg-slate-700">
+                     <CubeIcon className="w-6 h-6 text-slate-600 transition duration-75 dark:text-slate-400 group-hover:text-slate-900 dark:group-hover:text-white" fill="currentColor" stroke="currentColor" />
+                     <span className="sr-only">Item Types</span>
                   </Link>
                </Tooltip>
 
