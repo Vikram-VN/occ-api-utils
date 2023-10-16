1f33e80975134155710d7e15983c0802f93e88d3
Vikram-VN <vikram.narayanappa@apexit.com>
2023-10-16 15:55:08 +0530

    fixed visibility issue for header and footer

--- app/loading.jsx
+++ app/loading.jsx
@@ -5,12 +5,14 @@ import React from "react";
 const PageLoader = () => {
   return (
     <div className="text-center absolute z-10 grid w-full h-full content-center bg-slate-200 dark:bg-slate-900">
-      <Spinner
-        aria-label="Page Loader"
-        size="xl"
-        className="block"
-      />
-      <p>Please wait...</p>
+      <span>
+        <Spinner
+          aria-label="Page Loader"
+          size="xl"
+          className="block"
+        />
+        <p>Please wait...</p>
+      </span>
     </div>
   )
 }
