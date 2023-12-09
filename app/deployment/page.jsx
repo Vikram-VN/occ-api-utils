d50e2b396e6ccf576ce6dcc81defa3c60f969c15
Vikram N <104752629+jellyfish-17@users.noreply.github.com>
2023-12-09 11:26:17 +0530

    Fixed tabs issue

--- app/deployment/page.jsx
+++ app/deployment/page.jsx
@@ -18,22 +18,24 @@ export default function Deployments() {
   const actions = { router, toast, action };
 
   return (
-    <Tabs.Group
-      aria-label="Tabs with icons"
-      style="underline"
-      className="bg-white dark:bg-gray-800 rounded-md gap-4"
-    >
-      <Tabs.Item
-        title="Deployments History"
-        active={true}
-        icon={InboxArrowDownIcon}
+    <React.Fragment>
+      <Tabs
+        aria-label="Tabs with icons"
+        style="underline"
+        className="bg-white dark:bg-gray-800 rounded-md gap-4"
       >
-        <Deployment {...actions} />
-      </Tabs.Item>
+        <Tabs.Item
+          title="Deployments History"
+          active={true}
+          icon={InboxArrowDownIcon}
+        >
+          <Deployment {...actions} />
+        </Tabs.Item>
 
-      <Tabs.Item title="Publish History" icon={ServerIcon}>
-        <Publish {...actions} />
-      </Tabs.Item>
-    </Tabs.Group>
+        <Tabs.Item title="Publish History" icon={ServerIcon}>
+          <Publish {...actions} />
+        </Tabs.Item>
+      </Tabs>
+    </React.Fragment>
   );
 }
