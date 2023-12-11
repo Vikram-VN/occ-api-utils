536750bbc9b53d2ae318d414467d5aeb586515cd
Vikram N <104752629+jellyfish-17@users.noreply.github.com>
2023-12-11 21:29:29 +0530

    Fixed compilation loop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/flowbite-react/**/*.js",
     "./public/**/*.html",
   ],
